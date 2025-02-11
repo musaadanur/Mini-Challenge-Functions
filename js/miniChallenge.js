8062b213a7078d34bde994d761cc2ec8d30bae10
Musa Adanur <musa-adanur@hotmail.com>
2025-02-11 23:34:49 +0300

    sorular eklendi

---
+++ js/miniChallenge.js
@@ -0,0 +1,37 @@
+/*
+Function Declaration
+sayHello adında bir fonksiyon yazın.
+Bu fonksiyon bir isim parametresi almalı ve şu şekilde 
+bir selamlama mesajı döndürmeli: "Merhaba, [isim]!". 
+Fonksiyonu "Alice" ismiyle çağırın ve sonucu konsola yazdırın.
+*/
+
+/*
+Function Expression
+multiply isminde bir değişkene atanmış bir 
+fonksiyon ifadesi oluşturun. Bu fonksiyon iki 
+sayı almalı ve bu sayıların çarpımlarının sonucunu 
+döndürmeli. Fonksiyonu 5 ve 3 ile çağırın, 
+ardından sonucu konsola yazdırın.
+*/
+
+/*
+Arrow Functions
+divide adında bir ok fonksiyonu yazın. 
+Bu fonksiyon iki sayı almalı ve bu sayıların 
+bölme sonucunu döndürmeli. Fonksiyonu 10 ve 2 ile 
+test edin, sonucu konsola yazdırın.
+*/
+
+/*
+Varsayılan Parametreler
+greetUser adında bir fonksiyon oluşturun. 
+Bu fonksiyon bir name ve surname parametresi almalı. 
+name parametresinin varsayılan değeri "Misafir" olmalı.
+
+Fonksiyon "Hoş geldin, [isim] [soyisim]!" mesajını 
+döndürmeli. Bu mesajı + ile oluşturun.
+
+Fonksiyonu hem argüman olmadan hem de John Doe 
+ismiyle çağırarak test edin. Sonuçları konsola yazdırın.
+*/
